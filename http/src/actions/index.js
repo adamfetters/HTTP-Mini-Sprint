379958bdaf1004fbc7e575f7a377083cd8247912
Adam Fetters <adamfetterscoder@gmail.com>
2017-12-13 14:04:58 -0500

    Working - Did together in class

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -2,6 +2,17 @@ import axios from 'axios';
 
 export const GET_FRIENDS = 'GET_FRIENDS';
 
-export const getFriends = () => {
+export const getFriends = (friends) => {
     // Add the code for this action
+    const apiUrl = 'http://localhost:5000/friends';
+    const friendRequest = axios.get(apiUrl);
+       
+    // redux-promise middleware takes care of resolving the promise
+    return {
+        type: GET_FRIENDS,
+        payload: friendRequest
+    };
 };
+
+//enter redux-promise axios
+
